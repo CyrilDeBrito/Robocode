38151209f90dc1c313851dbcdb9f2d701aed31d9
Cyril De Brito <developpeur@cyrildebrito.info>
2019-12-09 10:22:16 +0100

    First step

---
+++ Robocode/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CDB
+{
+    using Robocode;
+    
+    public class MonPremierRobot
+    {
+        public override void Run()
+        {
+            // Perform your initialization for yout robot here
+
+            while (true)
+            {
+                // Perform robot here calling robot commands etc.
+            }
+        }
+    }
+}
